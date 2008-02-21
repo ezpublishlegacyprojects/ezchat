5e7f88242be8016c5a542a04dce5c7c85db5989d
xavier gouley <info@example.com>
2008-02-21 20:14:35 +0000

    Some documentation, ready to use
    git-svn-id: http://svn.projects.ez.no/ezchat@3 77717880-e582-498b-805b-c89642afe626

---
+++ trunk/extension/ezchat/doc/ezChat-technique.mm
@@ -0,0 +1,57 @@
+<map version="0.8.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1203511591756" ID="Freemind_Link_885457764" MODIFIED="1203511602914" TEXT="ezChat">
+<node CREATED="1203511607683" HGAP="21" ID="_" MODIFIED="1203511814076" POSITION="right" STYLE="bubble" TEXT="Extension" VSHIFT="1">
+<node CREATED="1203516183629" ID="Freemind_Link_1272774217" MODIFIED="1203526794930" TEXT="Default ajaxchat"/>
+<node CREATED="1203516259028" ID="Freemind_Link_482048088" MODIFIED="1203526817488" TEXT="Custom PHP classes">
+<arrowlink COLOR="#ff9966" DESTINATION="Freemind_Link_1272774217" ENDARROW="Default" ENDINCLINATION="29;-6;" ID="Freemind_Arrow_Link_654107948" STARTARROW="None" STARTINCLINATION="73;-16;"/>
+<node CREATED="1203526406471" HGAP="30" ID="Freemind_Link_728621260" LINK="../classes/langMap.php" MODIFIED="1203526483669" STYLE="fork" TEXT="Language mapping" VSHIFT="-7"/>
+<node CREATED="1203526459910" HGAP="31" ID="Freemind_Link_1835186606" LINK="../classes/config.php" MODIFIED="1203526485841" STYLE="fork" TEXT="dynamic configuration" VSHIFT="-6"/>
+</node>
+<node CREATED="1203516374438" ID="Freemind_Link_359943205" LINK="../translations/untranslated/translation.ts" MODIFIED="1203526684500" TEXT="Translations">
+<node CREATED="1203526659318" HGAP="25" ID="Freemind_Link_657081500" MODIFIED="1203527327708" STYLE="fork" TEXT="French" VSHIFT="7">
+<icon BUILTIN="pencil"/>
+</node>
+</node>
+<node CREATED="1203516351582" HGAP="180" ID="Freemind_Link_627092207" MODIFIED="1203527022524" TEXT="ezChat module" VSHIFT="35">
+<node CREATED="1203526946287" ID="Freemind_Link_1047021361" LINK="../modules/ezchat/room.php" MODIFIED="1203527271279" STYLE="fork" TEXT="room"/>
+<node CREATED="1203526965841" ID="Freemind_Link_1510843052" LINK="../modules/ezchat/welcome.php" MODIFIED="1203527463340" STYLE="fork" TEXT="welcome">
+<icon BUILTIN="pencil"/>
+</node>
+</node>
+<node CREATED="1203516346581" HGAP="181" ID="Freemind_Link_1022749331" MODIFIED="1203527031965" TEXT="Design" VSHIFT="44">
+<node CREATED="1203527353776" ID="Freemind_Link_1512096958" MODIFIED="1203527358459" STYLE="fork" TEXT="admin"/>
+<node CREATED="1203527395922" ID="Freemind_Link_806026599" MODIFIED="1203527400558" STYLE="fork" TEXT="ezflow"/>
+<node CREATED="1203527437366" ID="Freemind_Link_1139535931" MODIFIED="1203527448511" STYLE="fork" TEXT="grenland">
+<icon BUILTIN="pencil"/>
+</node>
+</node>
+<node CREATED="1203516365502" ID="Freemind_Link_888857787" MODIFIED="1203526870165" TEXT="Settings" VSHIFT="-20"/>
+<node CREATED="1203516338131" ID="Freemind_Link_1550787947" MODIFIED="1203516343398" TEXT="Datatypes">
+<node CREATED="1203526566650" ID="Freemind_Link_956486645" MODIFIED="1203526884146" STYLE="fork" TEXT="ezchatroom" VSHIFT="20"/>
+</node>
+</node>
+<node CREATED="1203511656433" ID="Freemind_Link_1520656581" MODIFIED="1203516430371" POSITION="left" STYLE="bubble" TEXT="Back Office" VSHIFT="-66">
+<node CREATED="1203511932180" HGAP="21" ID="Freemind_Link_1278675386" MODIFIED="1203527271279" STYLE="fork" TEXT="room" VSHIFT="-1">
+<arrowlink DESTINATION="Freemind_Link_1047021361" ENDARROW="Default" ENDINCLINATION="643;0;" ID="Freemind_Arrow_Link_113817611" STARTARROW="None" STARTINCLINATION="324;-277;"/>
+</node>
+<node CREATED="1203512708618" ID="Freemind_Link_1552119007" MODIFIED="1203527459578" STYLE="fork" TEXT="welcome">
+<arrowlink DESTINATION="Freemind_Link_1510843052" ENDARROW="Default" ENDINCLINATION="775;0;" ID="Freemind_Arrow_Link_1296066695" STARTARROW="None" STARTINCLINATION="427;-384;"/>
+<icon BUILTIN="pencil"/>
+</node>
+<node CREATED="1203512753228" ID="Freemind_Link_1967023400" MODIFIED="1203527313472" STYLE="fork" TEXT="logs">
+<icon BUILTIN="pencil"/>
+</node>
+</node>
+<node CREATED="1203511703749" ID="Freemind_Link_761741449" MODIFIED="1203516433303" POSITION="left" STYLE="bubble" TEXT="Front Office" VSHIFT="16">
+<node CREATED="1203511828694" ID="Freemind_Link_1750886009" MODIFIED="1203527238655" STYLE="fork" TEXT="Module view room" VSHIFT="-25">
+<arrowlink DESTINATION="Freemind_Link_1047021361" ENDARROW="Default" ENDINCLINATION="232;-352;" ID="Freemind_Arrow_Link_1863451149" STARTARROW="None" STARTINCLINATION="606;-439;"/>
+</node>
+<node CREATED="1203511845433" ID="Freemind_Link_1789388375" MODIFIED="1203526884146" STYLE="fork" TEXT="Attribute view ezchatroom" VSHIFT="48">
+<arrowlink DESTINATION="Freemind_Link_956486645" ENDARROW="Default" ENDINCLINATION="257;180;" ID="Freemind_Arrow_Link_156405193" STARTARROW="None" STARTINCLINATION="737;0;"/>
+<node CREATED="1203512054621" ID="Freemind_Link_539173851" MODIFIED="1203516151497" TEXT="full view"/>
+<node CREATED="1203513988645" ID="Freemind_Link_1649422957" MODIFIED="1203513993971" TEXT="shoutbox"/>
+</node>
+</node>
+</node>
+</map>
